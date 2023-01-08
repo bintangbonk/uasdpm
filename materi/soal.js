ae7cbe5a2e12a471e4c6b7804cc895fb81f2bab5
bintangbonk <bintangbongkys@gmail.com>
2023-01-09 05:21:15 +0700

    first commit

---
+++ materi/soal.js
@@ -0,0 +1,24 @@
+const DB_QUIZ = [
+    {
+        question : "Pertanyaan 1",
+        answers : ['jawaban1','jawaban2','jawaban3','jawaban4']
+    },
+    {
+        question : "Pertanyaan 1",
+        answers : ['jawaban1','jawaban2','jawaban3','jawaban4']
+    },
+    {
+        question : "Pertanyaan 1",
+        answers : ['jawaban1','jawaban2','jawaban3','jawaban4']
+    },
+    {
+        question : "Pertanyaan 1",
+        nswers : ['jawaban1','jawaban2','jawaban3','jawaban4']
+    },
+    {
+        question : "Pertanyaan 1",
+        answers : ['jawaban1','jawaban2','jawaban3','jawaban4']
+    }
+]
+
+const CORRECT_ANSWER = [1,3,4,2,1]
